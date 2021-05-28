77f6e559c1c3e229173f3321ec007a8282cad8ab
FeritBerkay <76204009+FeritBerkay@users.noreply.github.com>
2021-05-28 23:13:32 +0300

    Commit

--- notrhwind/src/main/java/kodlamaio/notrhwind/business/concretes/ProductManager.java
+++ notrhwind/src/main/java/kodlamaio/notrhwind/business/concretes/ProductManager.java
@@ -3,6 +3,9 @@ package kodlamaio.notrhwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.notrhwind.business.abstracts.ProductService;
@@ -28,13 +31,66 @@ public class ProductManager implements ProductService{
 	@Override //çiğnemek.
 	public DataResult<List<Product>> getAll() {
 		
-		return new SuccessDataResult<>(this.productDao.findAll(),"Data Listelendi.");
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data Listelendi.");
 	}
 
 	@Override
 	public Result add(Product product) {
 		this.productDao.save(product);
 		return new SuccessResult("Ürün eklendi");
+	}
+	
+	@Override
+	public DataResult<Product> getByProductName(String productName) {
+		return new SuccessDataResult<Product>
+		(this.productDao.getByProductName(productName),"Data listelendi");	
+	}
+
+	@Override
+	public DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId) {
+		//business codes
+		
+		return new SuccessDataResult<Product>
+		(this.productDao.getByProductNameAndCategory(productName,categoryId),"Data listelendi");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId) {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.getByProductNameOrCategory(productName, categoryId),"Data listelendi");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories) {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.getByCategoryIn(categories),"Data listelendi");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameContains(String productName) {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.getByProductNameContains(productName),"Data listelendi");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.getByProductNameStartsWith(productName),"Data listelendi");
+	}
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.findAll(pageable).getContent());
+	}
+
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		Sort sort = Sort.by(Sort.Direction.DESC,"productName");	//listelenecek ama hangi alana gore bu kısmı verdik.
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.findAll(sort),"Başarılı");
 	}
 	
 }
--- notrhwind/src/main/java/kodlamaio/notrhwind/dataAccess/abstracts/ProductDao.java
+++ notrhwind/src/main/java/kodlamaio/notrhwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,24 @@
 package kodlamaio.notrhwind.dataAccess.abstracts;
 
+import kodlamaio.notrhwind.core.Utilities.result.Result;
 import kodlamaio.notrhwind.entities.concretes.Product;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductDao extends JpaRepository<Product, Integer>{
+	Product getByProductName(String productName);
 	
+	Result add(Product product);
+	
+	 Product getByProductNameAndCategory(String productName, int categoryId);
+	  
+	  List<Product> getByProductNameOrCategory(String productName, int categoryId);
+	  
+	  List<Product> getByCategoryIn(List<Integer> categories);
+	  
+	  List<Product> getByProductNameContains(String productName);
+	  
+	  List<Product> getByProductNameStartsWith(String productName);
 }
